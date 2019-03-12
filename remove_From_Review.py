5de52a013c7e72b07f77b6b0d4df5229a0db621e
Nikhil Ashodariya <nikhilashodariya123@gmail.com>
2019-03-12 11:44:04 -0400

    Update remove_From_Review.py

--- remove_From_Review.py
+++ remove_From_Review.py
@@ -9,23 +9,40 @@ client = MongoClient(DB_HOST)
 db = client[DB_NAME]
 review_Collection = db[DB_COLLECTION]
 
-review_Cursor = review_Collection.find({})
+review_Cursor = review_Collection.find(no_cursor_timeout=False)
 
 
 business_Collection = db["business"]
 
 print("Code started successfully")
+
+skip = 0
+total_Count = review_Collection.find({}).count()
+limit = 10
 count = 0
 
-for doc in review_Cursor:
-    count = count + 1
-    print(count)
 
-    tempCursor = business_Collection.find({
-        "business_id": doc["business_id"]
-    })
+while count < total_Count:
+    # print(f"count of while = {count}")
+    review_Cursor = review_Collection.aggregate([
+                                                    {
+                                                        "$skip":skip
+                                                    },
+                                                    {
+                                                        "$limit":limit
+                                                    }
+                                                ])
 
-    if tempCursor.count() == 0:
-        review_Collection.delete_one(doc)
+    for doc in review_Cursor:
+        count = count + 1
+        print(f"count of for = {count}")
+        tempCursor = business_Collection.find({
+            "business_id":doc["business_id"]
+        })
 
+        if tempCursor.count() == 0:
+            review_Collection.delete_one(doc)
+
+
+    skip = skip + limit
 print("Code successfully exited")
