a62c39e5f7dfa9fdb1b0558bd179d28b6479b51b
KajolAch <42815235+KajolAch@users.noreply.github.com>
2019-03-30 14:51:05 -0400

    removed comments and added user id in the list

--- sentiment.py
+++ sentiment.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[172]:
-
-
 import numpy as np
 import pandas as pd
 import nltk
@@ -22,76 +16,46 @@ df = pd.read_csv("data/il_reviews.csv")
 df.drop(["cool","date","funny","useful"],axis = 1,inplace=True)
 
 
-processedData = list(df[["business_id","text"]].as_matrix())
+processedData = list(df[["business_id","text","user_id"]].as_matrix())
 
 WNlemma = nltk.WordNetLemmatizer()
 
-def funcTemp(x):
+def naive_sentiment(x):
     res = []
     ans = []
     positive_tokens=[]
     negative_tokens=[]
     negations=['not', 'too', 'n\'t', 'no', 'cannot', 'neither','nor']
     final_review=[]
-    ans.append(x[0])
+    final_review.append(x[0])
     data = x[1]
-      
+    final_review.append(x[2])  
     for val in sent_tokenize(data):
         val = val.strip(string.punctuation)
-        res.append([WNlemma.lemmatize(w.strip(string.punctuation)) for w in word_tokenize(val) if w.strip(string.punctuation)!=""])
-#         removed pos
+        
         for token in word_tokenize(val):
-            if token in positive_words:
-                    positive_tokens.append(token)
+            
+            if token in positive_words:   
+                  positive_tokens.append(token)
             else:
                   if token in negative_words:
                         negative_tokens.append(token)
                     
     if len(positive_tokens)>len(negative_tokens):
-        final_review.append("positive")
-#         positive_tokens.append([token for token in word_tokenize(val) \
-#                  if token in positive_words ])
-#         negative_tokens.append([token for token in word_tokenize(val) \
-#                  if token in negative_words])    
+        final_review.append("positive")  
     else:
         if len(negative_tokens)>len(positive_tokens):
             final_review.append("negative")
         else:
                 if len(positive_tokens)==len(negative_tokens):
                     final_review.append("neutral")  
-    ans.append(res)
-    print(final_review)
-#     stores final review sentiment
-#     d['ans']=ans
-#     d['final_review']=final_review
-    df.join(final_review)
-    return ans
-
-
-# In[ ]:
+    print(final_review)                
+    return final_review
 
 
 
 
-
-# In[173]:
-
+print(map(naive_sentiment,processedData))
 
 
-print(map(funcTemp,processedData))
-
-
-print(list(map(funcTemp,processedData))[0][1])
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+print(list(map(naive_sentiment,processedData))[0][1])
