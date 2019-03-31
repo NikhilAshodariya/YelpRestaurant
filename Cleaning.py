a93b67ecf28500888743e786028419b0776222e0
Nikhil Ashodariya <nikhilashodariya123@gmail.com>
2019-03-31 02:44:34 -0400

    [Nikhil] Modifying POS

--- Cleaning.py
+++ Cleaning.py
@@ -12,25 +12,19 @@ df = pd.read_csv("data/il_reviews.csv")
 df.drop(["cool","date","funny","useful"],axis = 1,inplace=True)
 
 
-processedData = list(df[["business_id","text"]].as_matrix())
+processedData = df.text.tolist()
 
 WNlemma = nltk.WordNetLemmatizer()
 
-def funcTemp(x):
-    res = []
+def tokenize_Lemmatize_POS(data):
     ans = []
-    ans.append(x[0])
-    data = x[1]
-    
     for val in sent_tokenize(data):
         val = val.strip(string.punctuation)
-        res.append(pos_tag([WNlemma.lemmatize(w.strip(string.punctuation)) for w in word_tokenize(val) if w.strip(string.punctuation)!=""]))
-    
-	ans.append(res)
-    return ans
+        ans.extend([WNlemma.lemmatize(w.strip(string.punctuation)) for w in word_tokenize(val) if w.strip(string.punctuation)!=""])
+    return pos_tag(ans)
 
-print(map(funcTemp,processedData))
+print(map(tokenize_Lemmatize_POS,processedData))
 
 
-print(list(map(funcTemp,processedData))[0][1])
-
+tagged_POS = list(map(tokenize_Lemmatize_POS,processedData))
+tagged_POS_POS[:5]
